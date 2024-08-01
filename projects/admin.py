4a84df8289d869dc7715aeac8a21f9ff9c0e7c80
suhail-n <suhail.nooristani@gmail.com>
2024-08-01 00:43:05 -0400

    feat: :sparkles: register all models to the admin interface
    The following models are added to the admin interface
    -Project
    -FeatureFlag
    -Toggle

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Project, FeatureFlag, Toggle
 # Register your models here.
+
+
+admin.site.register(Project)
+admin.site.register(FeatureFlag)
+admin.site.register(Toggle)
