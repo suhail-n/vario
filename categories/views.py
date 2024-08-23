c0413805eb776675c86bc27dfd1a7fe65d48babc
suhail-n <suhail.nooristani@gmail.com>
2024-08-23 08:48:16 -0400

    feat: :sparkles: Create Feature flag categories app
    - Category app renamed to Categories

---
+++ categories/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from categories.models import Category
+
+
+# Create your views here.
+
+
+class ListCategories(ListView):
+    # template name default will be category_list.html
+    model = Category
+    context_object_name = "categories"
